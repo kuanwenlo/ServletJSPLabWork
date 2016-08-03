cacac2b416b5ed3a64aa9d96d80f3e2ea112bc35
kualo <kualo@visa.com>
2016-08-03 09:34:48 -0500

    2Lab

---
+++ src/ZellerCongruence.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+/**
+ * Created by kualo on 8/2/2016.
+ */
+
+public class ZellerCongruence {
+
+    private static String tellDay(int year, int month, int day) {
+
+        int a = (int) (13 * (month + 1) / 5);
+        int b = (int) (year / 4);
+        int c = 6 * (int) (year / 100);
+        int d = (int) (year / 400);
+        int sum = a + b + c + d;
+
+        int result = sum % 7;
+
+        switch(result) {
+            case 0:
+                return "Sunday";
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+        }
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = Integer.valueOf(sc.nextInt());
+        int month = Integer.valueOf(sc.nextInt());
+        int day = Integer.valueOf(sc.nextInt());
+
+        String date = tellDay(year, month, day);
+
+        System.out.println("The date is :" + date);
+
+
+
+    }
+}
